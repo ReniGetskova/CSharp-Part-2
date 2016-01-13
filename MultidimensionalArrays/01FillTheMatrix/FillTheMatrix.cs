fd58562435b075c53946ebfaeeab0e33bb7db580
ReniGetskova <reni.getskova@gmail.com>
2016-01-13 15:06:31 +0200

    Added examples as comments

--- MultidimensionalArrays/01FillTheMatrix/FillTheMatrix.cs
+++ MultidimensionalArrays/01FillTheMatrix/FillTheMatrix.cs
@@ -1,4 +1,29 @@
 ﻿// Write a program that fills and prints a matrix of size (n, n) 
+/*
+a)	
+1	5	9	13
+2	6	10	14
+3	7	11	15
+4	8	12	16
+
+b)
+1	8	9	16
+2	7	10	15
+3	6	11	14
+4	5	12	13
+
+c)
+7	11	14	16
+4	8	12	15
+2	5	9	13
+1	3	6	10
+
+d)
+1	12	11	10
+2	13	16	9
+3	14	15	8
+4	5	6	7
+*/
 
 using System;
 
@@ -124,7 +149,6 @@ class FillTheMatrix
                 matrix[row + col, col] = number++;
             }
         }
-
         for (int col = 1; col < size; col++)
         {
             for (int row = 0; row < size - col; row++)
